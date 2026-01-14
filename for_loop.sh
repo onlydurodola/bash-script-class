6b503eaa350c7e4fb246942bcf3dcf18f66c3288
onlydurodola <nowonlywork@gmail.com>
2026-01-14 20:11:17 +0100

    for loop examples

---
+++ for_loop.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+students="Hunter Bobby Precious Sixtus Seun Adejuyitan"
+
+for student in ${students}
+# without variable for student in Hunter Bobby Precious Sixtus Seun Adejuyitan
+do
+    echo "Present: $student"
+    sleep 3 
+done
+
+echo "Attendance Complete"
+
+echo "DONEEEE"
+
+for number in {1..10}
+do
+    echo ${number}
+    sleep 3
+done
+
